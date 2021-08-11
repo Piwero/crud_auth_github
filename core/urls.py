77387176f94b73749b10310ff48f41e6cdff5a0d
Piwero <piwero@gmail.com>
2021-08-11 15:31:18 +0100

    create templates with views for sign up, log in and edit profile

--- core/urls.py
+++ core/urls.py
@@ -19,14 +19,12 @@ from django.urls import (
     path,
 )
 
-from core.views import (
-    home,
-    login,
-)
+from core.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("accounts/", include("allauth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", home, name="home"),
-    path("login", login, name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,7 +4,3 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, "home.html")
-
-
-def login(request):
-    return render(request, "login.html")
