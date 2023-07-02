db7963994c9dae26d9c9b29e28e5e03c569afe55
Nacho Pinero <piwero@gmail.com>
2023-07-02 18:42:12 +0100

    Move DB setup to production_settings.py

--- core/settings/base_settings.py
+++ core/settings/base_settings.py
@@ -69,20 +69,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
--- core/settings/production_settings.py
+++ core/settings/production_settings.py
@@ -1,1 +1,12 @@
-from core.settings.base_settings import * # NOQA isort:skip
+from core.settings.base_settings import *  # NOQA isort:skip
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
+    }
+}
