986ac378dd6ad54f1a4d789ccf2892509677df26
Nacho Pinero <piwero@gmail.com>
2023-07-02 18:55:03 +0100

    Fix test settings DB

--- core/settings/test_settings.py
+++ core/settings/test_settings.py
@@ -1,3 +1,3 @@
 from core.settings.base_settings import *  # NOQA isort:skip
 
-DATABASES["default"] = {"ENGINE": "django.db.backends.sqlite3"}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
