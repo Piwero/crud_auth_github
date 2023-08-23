bb6f4aef9c64e15af0938e56fb1e73d999858b7f
Nacho Pinero <piwero@gmail.com>
2023-08-23 17:23:43 +0100

    Rename postgres service

--- core/settings/production_settings.py
+++ core/settings/production_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
+        "HOST": "postgres-db-service",  # set in docker-compose.yml and K8s service
         "PORT": 5432,  # default postgres port
     }
 }
